5c47d33dbb4258b14f2fb315f0c491898004508c
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-08-29 22:13:27 +0100

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""empty class Square that defines a square"""
+
+
 class Square:
-    """an empty class Square that defines a square:"""
+    """empty class Square that defines a square"""
     pass
