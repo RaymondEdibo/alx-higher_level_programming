82c6f5f49c9247962c60040d52d1a523d8e29d00
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-09-12 22:38:49 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""class Student which defines student"""
+"""Student module.
+
+Contains a Student class and some methods.
+"""
 
 
-class Student:
-    """student data"""
+class Student():
+    """Defines a Student."""
 
     def __init__(self, first_name, last_name, age):
+        """Sets the necessary attributes for the Student object.
+
+        Args:
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """return dictionary"""
-
-        if attrs is None or type(attrs) is not list:
-                return self.__dict__
-        else:
-            temp_list = {}
-            for element in attrs:
-                if type(element) is not str:
-                    return self.__dict__
-                if element in self.__dict__.keys():
-                    temp_list[element] = self.__dict__[element]
-            return temp_list
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is not None:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
