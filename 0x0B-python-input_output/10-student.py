8786e8ff9713d8fbd36de93c8865011134177748
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-09-12 22:39:58 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 """Student module.
 
-Contains a Student class and some methods.
+Student class
 """
 
 
 class Student():
-    """Defines a Student."""
+    """Defines Student."""
 
     def __init__(self, first_name, last_name, age):
-        """Sets the necessary attributes for the Student object.
-
-        Args:
-            first_name (str): first name of the student.
-            last_name (str): last name of the student.
-            age (int): age of the student.
+        """
+        Student object.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance."""
+        """Dictionary representation"""
         if attrs is not None:
             return {k: v for k, v in self.__dict__.items() if k in attrs}
         return self.__dict__
